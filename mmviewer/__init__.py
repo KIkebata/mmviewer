e6020816a4bf59c00047b306ee055fce3b07faeb
KIkebata <ken.5.k.u.4991@gmail.com>
2021-12-29 16:22:00 +0900

    edit __init__.py

--- mmviewer/__init__.py
+++ mmviewer/__init__.py
@@ -1,2 +1,8 @@
 #!/usr/bin/env python3
 from __version__ import __version__
+
+__copyright__ = 'Copyright (C) 2022 Kengo Ikebata'
+__license__ = 'GPLv3'
+__author__ = 'Kengo Ikebata'
+__author_email__ = 'kikebata623@gmail.com'
+__url__ = 'https://github.com/KIkebata/mmviewer'
